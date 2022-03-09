d2564198e7a7c64655f212c18d7153346b6e3599
ManjuV <vmanju85@gmail.com>
2022-03-09 14:27:08 +0530

    Initial Commit

---
+++ VehicleAppUITests/VehicleAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VehicleAppUITestsLaunchTests.swift
+//  VehicleAppUITests
+//
+//  Created by manju V on 09/03/22.
+//
+
+import XCTest
+
+class VehicleAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
